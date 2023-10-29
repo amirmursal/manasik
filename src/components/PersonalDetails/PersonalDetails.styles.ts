864cb25fc8e8ed4ff68c0158316e78972c740a71
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-10-29 21:01:55 +0530

    changes 2.0

--- src/components/PersonalDetails/PersonalDetails.styles.ts
+++ src/components/PersonalDetails/PersonalDetails.styles.ts
@@ -6,13 +6,13 @@ const personalDetailsStyles = {
         width:"90%",
         margin:"auto",
         marginTop:"1%",
-    },
+        },
     head:{
         margin:"auto",
         color: '#4CBB17',
         display:"flex",
         justifyContent:"center",
-        fontWeight:"600"
+        fontWeight:"600",
     },
     subHead:{
         color: '#000',
@@ -20,7 +20,8 @@ const personalDetailsStyles = {
         marginBottom:"4vh",
         display:"flex",
         justifyContent:"center",
-        textAlign: 'center'
+        textAlign: 'center',
+        boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;",
     },
     input:{
         display:"flex",
--- src/components/PersonalDetails/PersonalDetails.tsx
+++ src/components/PersonalDetails/PersonalDetails.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import personalDetailsStyles from "./PersonalDetails.styles";
 import { Box, Button, FormControl, TextField, Grid, Divider, InputLabel, MenuItem, Select, Typography } from "@mui/material";
-
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 const PersonalDetails = () => {
   return (
@@ -14,17 +16,26 @@ const PersonalDetails = () => {
 
       <Box sx={{ width: "90%", margin: "auto", display: 'flex', flexDirection: "column", alignItems: 'center' }}>
 
-
         <Grid container rowSpacing={7} columns={{ xs: 1, sm: 4, md: 12 }}  columnSpacing={{ xs: 1, sm: 12, md: 12}}>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
-            <Typography sx={personalDetailsStyles.lable} >Travels Name </Typography>
-            <TextField type='text' label='Enter Travels Name' size="small" sx={personalDetailsStyles.text} />
+            <Typography sx={personalDetailsStyles.lable} >Agency Name</Typography>
+            <TextField type='text' label='Enter Agency Name' size="small" sx={personalDetailsStyles.text} />
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
-            <Typography sx={personalDetailsStyles.lable} >Owner Number </Typography>
-            <TextField type='text' label='Enter Owner Name' size="small" sx={personalDetailsStyles.text} />
+            <Typography sx={personalDetailsStyles.lable} >Owner First Name </Typography>
+            <TextField type='text' label='Enter Owner First Name' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Owner Middle Name </Typography>
+            <TextField type='text' label='Enter Owner First Name' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Owner Last Name </Typography>
+            <TextField type='text' label='Enter Owner First Name' size="small" sx={personalDetailsStyles.text} />
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
@@ -50,17 +61,39 @@ const PersonalDetails = () => {
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
             <Typography sx={personalDetailsStyles.lable} >Country </Typography>
-            <TextField type='text' label='Enter Country Name' size="small" sx={personalDetailsStyles.text} />
+            <FormControl variant="outlined">
+              <InputLabel>Country</InputLabel>
+              <Select size="small" label='select' sx={personalDetailsStyles.text} >
+                <MenuItem value="India">India</MenuItem>
+                <MenuItem value="Bangladesh">Bangladesh</MenuItem>
+                <MenuItem value="Pakistan">Pakistan</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
             <Typography sx={personalDetailsStyles.lable} >State </Typography>
-            <TextField type='text' label='Enter State' size="small" sx={personalDetailsStyles.text} />
+            <FormControl variant="outlined">
+              <InputLabel>Country</InputLabel>
+              <Select size="small" label='select' sx={personalDetailsStyles.text} >
+                <MenuItem value="Maharashtra">Maharashtra</MenuItem>
+                <MenuItem value="Telengana">Telengana</MenuItem>
+                <MenuItem value="Karnataka">Karnataka</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
             <Typography sx={personalDetailsStyles.lable} >District </Typography>
-            <TextField type='text' label='Enter District' size="small" sx={personalDetailsStyles.text} />
+            <FormControl variant="outlined">
+              <InputLabel>District</InputLabel>
+              <Select size="small" label='select' sx={personalDetailsStyles.text} >
+                <MenuItem value="Pune">Pune</MenuItem>
+                <MenuItem value="Latur">Latur</MenuItem>
+                <MenuItem value="Aurangabad">Aurangabad</MenuItem>
+                <MenuItem value="Usmanabad">Usmanabad</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
@@ -70,14 +103,7 @@ const PersonalDetails = () => {
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
             <Typography sx={personalDetailsStyles.lable} >Address </Typography>
-            <TextField
-          type='text'
-          label='Enter Address'
-          multiline
-          rows={2}
-         
-          sx={personalDetailsStyles.text}
-        />
+            <TextField type='text' label='Enter Address' multiline rows={2} sx={personalDetailsStyles.text} />
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
@@ -101,8 +127,18 @@ const PersonalDetails = () => {
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
-            <Typography sx={personalDetailsStyles.lable} >Pan</Typography>
-            <TextField type='text' label='Enter Pan Number' size="small" sx={personalDetailsStyles.text} />
+            <Typography sx={personalDetailsStyles.lable} >Agency PAN</Typography>
+            <TextField type='text' label='Enter Agency PAN' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >GSTN</Typography>
+            <TextField type='text' label='Enter GSTN' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Owner Aadhar</Typography>
+            <TextField type='text' label='Enter Owner Aadhar' size="small" sx={personalDetailsStyles.text} />
           </Grid>
 
           <Grid item xs={6} sx={personalDetailsStyles.input}>
@@ -111,7 +147,7 @@ const PersonalDetails = () => {
               <InputLabel>select</InputLabel>
               <Select size="small" label='select' sx={personalDetailsStyles.text} >
                 <MenuItem value="pune">Yes</MenuItem>
-                <MenuItem value="pune">Now</MenuItem>
+                <MenuItem value="pune">No</MenuItem>
               </Select>
             </FormControl>
           </Grid>
@@ -121,16 +157,67 @@ const PersonalDetails = () => {
             <TextField type='number' label='Enter MSME Number' size="small" sx={personalDetailsStyles.text} />
           </Grid>
 
-        </Grid>
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Status</Typography>
+            <FormControl variant="outlined">
+              <InputLabel>Status</InputLabel>
+              <Select size="small" label='Status' sx={personalDetailsStyles.text} >
+                <MenuItem value="pune">Active</MenuItem>
+                <MenuItem value="pune">In-Active</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Onboarding Date</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker label='On boarding Date' sx={personalDetailsStyles.text} />
+             </LocalizationProvider>
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Termination Date</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker label='Termination Date' sx={personalDetailsStyles.text} />
+             </LocalizationProvider>
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Created By</Typography>
+            <TextField type='number' label='CreatedBy' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Created Date</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker label='Created Date' sx={personalDetailsStyles.text} />
+             </LocalizationProvider>
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Last Modified Date</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker label='Last Modified Date' sx={personalDetailsStyles.text} />
+             </LocalizationProvider>
+          </Grid>
+
+          <Grid item xs={6} sx={personalDetailsStyles.input}>
+            <Typography sx={personalDetailsStyles.lable} >Last Modified By</Typography>
+            <TextField type='number' label='Last Modified By' size="small" sx={personalDetailsStyles.text} />
+          </Grid>
+
+      
 
         <Grid marginTop="5%" marginBottom="1%" display='flex' gap="5px"  >
           <Button href="/addAgentBankDetails" variant="contained">Next Page</Button>
         </Grid>
 
+      </Grid>
 
       </Box>
 
       <Divider sx={personalDetailsStyles.divider} />
+
     </Box>
   );
 }
