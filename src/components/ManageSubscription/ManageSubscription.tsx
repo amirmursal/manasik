e63d97ee0936407d043d5fb4ebbb8ed1f19e2399
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-11-21 20:51:14 +0530

    second commit

--- src/components/ManageSubscription/ManageSubscription.tsx
+++ src/components/ManageSubscription/ManageSubscription.tsx
@@ -25,35 +25,35 @@ const ManageSubscription = () => {
             component="div"
             sx={ManageSubscriptionStyles.preSubHead}
           >
-            UMRAH 30-SEP-2023
+            Add Subscription Plan
           </Typography>
        
           <Box sx={ManageSubscriptionStyles.program}>
             <Grid
               container
-              columns={{ xs: 1, sm: 8 , md: 12 }}
+              columns={{ xs: 1, sm: 4.6 , md: 12 }}
             >
-              <Grid item xs={4} sx={ManageSubscriptionStyles.fair}>
+              <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
                 <Typography variant="h6">Seat Count</Typography>
                 <TextField type="number" size="small" />
               </Grid>
     
-              <Grid item xs={4} sx={ManageSubscriptionStyles.fair}>
+              <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
                 <Typography variant="h6">30 Days Cost</Typography>
                 <TextField type="number" size="small" />
               </Grid>
     
-              <Grid item xs={4} sx={ManageSubscriptionStyles.fair}>
+              <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
                 <Typography variant="h6">90 Days Cost</Typography>
                 <TextField type="number" size="small" />
               </Grid>
     
-              <Grid item xs={4} sx={ManageSubscriptionStyles.fair}>
+              <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
                 <Typography variant="h6">180 Days Cost</Typography>
                 <TextField type="number" size="small" />
               </Grid>
     
-              <Grid item xs={8} sx={ManageSubscriptionStyles.seats}>
+              <Grid item xs={2.8} sx={ManageSubscriptionStyles.seats}>
                 <Typography variant="h6">360 Days Cost</Typography>
                 <TextField type="number" size="small" />
                 <Icon sx={ManageSubscriptionStyles.icon}>+</Icon>
@@ -65,11 +65,11 @@ const ManageSubscription = () => {
               <thead>
                 <tr>
                   <th style={{ backgroundColor: "white" }}></th>
-                  <th>Package Type</th>
-                  <th>Total Fare (INR)</th>
-                  <th>Max Seat Count</th>
-                  <th>Seat Booked</th>
-                  <th>Seat Available</th>
+                  <th>Seat Count</th>
+                  <th>30 Days</th>
+                  <th>90 Days</th>
+                  <th>180 Days</th>
+                  <th>360 Days</th>
                   <th style={{ backgroundColor: "white" }}></th>
                 </tr>
               </thead>
@@ -78,33 +78,33 @@ const ManageSubscription = () => {
                   <td>
                     <CreateIcon />
                   </td>
-                  <td>Economy</td>
-                  <td>80,000</td>
-                  <td>30</td>
-                  <td>25</td>
-                  <td>5</td>
+                  <td>50</td>
+                  <td>5000</td>
+                  <td>10000</td>
+                  <td>15000</td>
+                  <td>22500</td>
                   <td>✕</td>
                 </tr>
                 <tr>
                   <td>
                     <CreateIcon />
                   </td>
-                  <td>Delux</td>
-                  <td>90,000</td>
-                  <td>10</td>
-                  <td>8</td>
-                  <td>2</td>
+                  <td>100</td>
+                  <td>47500</td>
+                  <td>15000</td>
+                  <td>22500</td>
+                  <td>33750</td>
                   <td>✕</td>
                 </tr>
                 <tr>
                   <td>
                     <CreateIcon />
                   </td>
-                  <td>Super Delux</td>
-                  <td>100,000</td>
-                  <td>10</td>
-                  <td>7</td>
-                  <td>3</td>
+                  <td>5000</td>
+                  <td>16875</td>
+                  <td>33750</td>
+                  <td>50625</td>
+                  <td>75938</td>
                   <td>✕</td>
                 </tr>
               </tbody>
