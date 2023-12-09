8d6d98d3268f0a42f15368fc7891825a96fedcf8
amir.mursal <amir.mursal@nuance.com>
2023-12-09 19:05:18 +0530

    feat: added material ui data grid into add program component

--- src/components/AddProgram/AddProgram.tsx
+++ src/components/AddProgram/AddProgram.tsx
@@ -1,127 +1,203 @@
-import React from "react";
-import AddProgramStyles from "./AddProgram.styles";
-import PickerWithButtonField from "./DatePicker";
+import React, { useState } from "react";
+import addProgramStyles from "./AddProgram.styles";
 import {
   Box,
   TextField,
   InputLabel,
   FormControl,
-  Divider,
   Grid,
   MenuItem,
   Select,
+  Button,
+  IconButton,
   Typography,
 } from "@mui/material";
-import Icon from "@mui/material/Icon";
-import CreateIcon from "@mui/icons-material/Create";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const AddProgram = () => {
+  const [show, setShow] = useState(false);
+
+  const handleAddProgram = () => {
+    setShow((previous) => !previous);
+  };
+
+  const handleAddButtonClick = () => {
+    console.log("Button Clicked");
+  };
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 200 },
+    { field: "programType", headerName: "Program Type", width: 200 },
+    { field: "travelDate", headerName: "Travel Date", width: 200 },
+    {
+      field: "programStatus",
+      headerName: "Program Status",
+      width: 200,
+    },
+    {
+      field: "maxSeatCount",
+      headerName: "Max Seat Count",
+      width: 200,
+    },
+    {
+      field: "seatBooked",
+      headerName: "Seat Booked",
+      width: 197,
+    },
+    {
+      field: "seatAvailable",
+      headerName: "Seat Available",
+      width: 200,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      travelDate: "12/10/2024",
+      programType: "Umrah",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 2,
+      travelDate: "12/10/2024",
+      programType: "Hajj",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 3,
+      travelDate: "12/10/2024",
+      programType: "Umrah",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 4,
+      travelDate: "12/10/2024",
+      programType: "Umrah",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 5,
+      travelDate: "12/10/2024",
+      programType: "Umrah",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 6,
+      travelDate: "12/10/2024",
+      programType: "Hajj",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 7,
+      travelDate: "12/10/2024",
+      programType: "Hajj",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 8,
+      travelDate: "12/10/2024",
+      programType: "Hajj",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+    {
+      id: 9,
+      travelDate: "12/10/2024",
+      programType: "Hajj",
+      programStatus: "Close",
+      maxSeatCount: 50,
+      seatBooked: 10,
+      seatAvailable: 5,
+    },
+  ];
+
   return (
-    <Box sx={AddProgramStyles.root}>
-      <Typography variant="h4" component="div" sx={AddProgramStyles.head}>
-        Manage Programs
-      </Typography>
-
-      <Divider sx={AddProgramStyles.divider} />
-
-      <Typography variant="h6" component="div" sx={AddProgramStyles.subHead}>
-        Add Program
-      </Typography>
-
-      <Box sx={AddProgramStyles.program}>
-        <Grid container columns={{ xs: 1, sm: 6, md: 12 }}>
-          <Grid xs={3} sx={AddProgramStyles.pad}>
-            <FormControl
-              sx={AddProgramStyles.formcontrol}
-              fullWidth
-              variant="outlined"
-            >
-              <InputLabel sx={AddProgramStyles.label}>Program Type</InputLabel>
-              <Select sx={AddProgramStyles.selector}>
+    <Box>
+      <Box sx={addProgramStyles.addProgramButton}>
+        <Typography variant="h6">Balance Subscription: 50</Typography>
+        <Button color="primary" variant="contained" onClick={handleAddProgram}>
+          Add Program
+        </Button>
+      </Box>
+      {show && (
+        <Grid container spacing={1} sx={addProgramStyles.addProgramForm}>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Program Type
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Program Type"
+              >
                 <MenuItem value="hajj">Hajj</MenuItem>
                 <MenuItem value="umrah">Umrah</MenuItem>
               </Select>
             </FormControl>
           </Grid>
-
-          <Grid item xs={3} sx={AddProgramStyles.pad}>
-            <PickerWithButtonField />
+          <Grid item xs={2}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker label="Travel Date" onChange={(newValue) => {}} />
+            </LocalizationProvider>
           </Grid>
-
-          <Grid item xs={6} sx={AddProgramStyles.seats}>
-            <Typography variant="h6">Max Seats Count</Typography>
-            <TextField type="number" size="small" />
-            <Icon sx={AddProgramStyles.icon}>+</Icon>
+          <Grid item xs={3}>
+            <TextField
+              fullWidth
+              type="number"
+              label="Seat Count"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <IconButton color="primary" onClick={handleAddButtonClick}>
+              <AddCircleIcon />
+            </IconButton>
           </Grid>
         </Grid>
-      </Box>
-      <Box sx={AddProgramStyles.table}>
-        <table>
-          <thead>
-            <tr>
-              <th style={{ backgroundColor: "white" }}></th>
-              <th>Pogram Type</th>
-              <th>Travel Date</th>
-              <th>Pogram Status</th>
-              <th>Max Seat Count</th>
-              <th>Seat Booked</th>
-              <th>Seat Available</th>
-              <th style={{ backgroundColor: "white" }}></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>UMRAH</td>
-              <td>30-Sep-2023</td>
-              <td>Ready for Travel</td>
-              <td>50</td>
-              <td>50</td>
-              <td>o</td>
-              <td>✕</td>
-            </tr>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>HAJJ</td>
-              <td>15-July-2024</td>
-              <td>Booking In Progress</td>
-              <td>200</td>
-              <td>20</td>
-              <td>180</td>
-              <td>✕</td>
-            </tr>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>UMRAH</td>
-              <td>10-Apr-2023</td>
-              <td>Closed</td>
-              <td>50</td>
-              <td>45</td>
-              <td>5</td>
-              <td>✕</td>
-            </tr>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>UMRAH</td>
-              <td>05-May-2023</td>
-              <td>Canceled</td>
-              <td>50</td>
-              <td>45</td>
-              <td>5</td>
-              <td>✕</td>
-            </tr>
-          </tbody>
-        </table>
-      </Box>
-      <Divider sx={AddProgramStyles.divider} />
+      )}
+      <br />
+      <DataGrid
+        keepNonExistentRowsSelected
+        sx={addProgramStyles.root}
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        rowSelection={false}
+        pageSizeOptions={[5, 10]}
+      />
     </Box>
   );
 };
