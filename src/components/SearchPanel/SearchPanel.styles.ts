5d31b31861a2db48482a5fb3269ef8bbdaaf3d76
amir.mursal <amir.mursal@nuance.com>
2023-12-10 12:08:22 +0530

    fix: add search panel style to align items

--- src/components/SearchPanel/SearchPanel.styles.ts
+++ src/components/SearchPanel/SearchPanel.styles.ts
@@ -1,10 +1,19 @@
 const searchPanelStyles = {
-    root:{
-        color: '#000'
-    },
-    infoText:{
-        margin: '5%',
-        textAlign: 'center'
-    }
-}
+  root: {
+    color: "#000",
+  },
+  infoText: {
+    margin: "5%",
+    textAlign: "center",
+  },
+  container: {
+    margin: "5%",
+    display: "flex",
+    alignItems: "center",
+  },
+  searchForm: {
+    display: "flex",
+    alignItems: "center",
+  },
+};
 export default searchPanelStyles;
--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -1,49 +1,63 @@
-import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 import searchPanelStyles from "./SearchPanel.styles";
 import React from "react";
 
 const SearchPanel = () => {
-
-    return (
-        <Box sx={searchPanelStyles.root}>
-            <Typography variant="h6" sx={searchPanelStyles.infoText}
-                component="div">Find the Best & Reliable HAJJ and UMRAH Tours in India
-            </Typography>
-            <Box sx={{ margin: '5%', display:'flex', alignItems:'center' }}>
-                <Grid container spacing={2} columns={{xs:1, sm:8, md:12}}>
-                    <Grid item xs={3}>
-                    <FormControl fullWidth variant="outlined">
-                            <InputLabel>Tour Type</InputLabel>
-                            <Select label="Tour Type">
-                                <MenuItem value="hajj">Hajj</MenuItem>
-                                <MenuItem value="umrah">Umrah</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormControl fullWidth variant="outlined">
-                            <InputLabel>State</InputLabel>
-                            <Select label="State">
-                                <MenuItem value="maharashtra">Maharashtra</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormControl fullWidth variant="outlined">
-                            <InputLabel>City</InputLabel>
-                            <Select label="City">
-                                <MenuItem value="pune">Pune</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={3}>
-                    <FormControl variant="outlined">
-                        <Button variant="contained">Search Tour</Button>
-                        </FormControl>
-                    </Grid>
-                </Grid></Box>
-        </Box>
-    )
-}
+  return (
+    <Box sx={searchPanelStyles.root}>
+      <Typography variant="h6" sx={searchPanelStyles.infoText} component="div">
+        Find the Best & Reliable HAJJ and UMRAH Tours in India
+      </Typography>
+      <Box sx={searchPanelStyles.container}>
+        <Grid
+          container
+          spacing={2}
+          columns={{ xs: 1, sm: 8, md: 12 }}
+          sx={searchPanelStyles.searchForm}
+        >
+          <Grid item xs={3}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>Tour Type</InputLabel>
+              <Select label="Tour Type">
+                <MenuItem value="hajj">Hajj</MenuItem>
+                <MenuItem value="umrah">Umrah</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>State</InputLabel>
+              <Select label="State">
+                <MenuItem value="maharashtra">Maharashtra</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>City</InputLabel>
+              <Select label="City">
+                <MenuItem value="pune">Pune</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl variant="outlined">
+              <Button variant="contained">Search Tour</Button>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
 
 export default SearchPanel;
