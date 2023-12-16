87e40682c2f4e4fdb6fd789a418c0b312b8910e8
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-12-17 03:09:17 +0530

    make the dinamic

--- src/components/Agent/ManageSubscription/ManageSubscription.tsx
+++ src/components/Agent/ManageSubscription/ManageSubscription.tsx
@@ -1,101 +1,235 @@
-import React from "react";
+import React, { useState } from "react";
 import ManageSubscriptionStyles from "./ManageSubscription.styles";
-import { Box, TextField, Divider, Grid, Typography } from "@mui/material";
-
-import Icon from "@mui/material/Icon";
-import CreateIcon from "@mui/icons-material/Create";
+import {
+  Box,
+  TextField,
+  InputLabel,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+  Button,
+  IconButton,
+} from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import manageSubscriptionStyles from "./ManageSubscription.styles";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const ManageSubscription = () => {
+  const [show, setShow] = React.useState(false);
+  const [subscriptionType, setSubscriptionType] = useState("");
+
+  const handleManageSubscription = () => {
+    setShow((previous) => !previous);
+  };
+
+  const handleAddButtonClick = () => {
+    console.log("Button Clicked");
+  };
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 210 },
+    { field: "seatCount", headerName: "Seat Count", width: 230 },
+    { field: "days30", headerName: "30 Days", width: 230 },
+
+    {
+      field: "days90",
+      headerName: "90 Days",
+      width: 230,
+    },
+    {
+      field: "days180",
+      headerName: "180 Days",
+      width: 230,
+    },
+    {
+      field: "days360",
+      headerName: "360 Days",
+      width: 230,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      seatCount: "50",
+      days30: "5000",
+      days90: "10000",
+      days180: "15000",
+      days360: "20000",
+    },
+    {
+      id: 2,
+      seatCount: "100",
+      days30: "4000",
+      days90: "8000",
+      days180: "12000",
+      days360: "16000",
+    },
+    {
+      id: 3,
+      seatCount: "200",
+      days30: "3000",
+      days90: "6000",
+      days180: "9000",
+      days360: "13000",
+    },
+    {
+      id: 4,
+      seatCount: "500",
+      days30: "6000",
+      days90: "12000",
+      days180: "18000",
+      days360: "22000",
+    },
+    {
+      id: 5,
+      seatCount: "700",
+      days30: "7000",
+      days90: "14000",
+      days180: "19000",
+      days360: "25000",
+    },
+    {
+      id: 6,
+      seatCount: "500",
+      days30: "6500",
+      days90: "13200",
+      days180: "19700",
+      days360: "26700",
+    },
+    {
+      id: 7,
+      seatCount: "1500",
+      days30: "3100",
+      days90: "6400",
+      days180: "11500",
+      days360: "17400",
+    },
+    {
+      id: 8,
+      seatCount: "2300",
+      days30: "4700",
+      days90: "9600",
+      days180: "13900",
+      days360: "21600",
+    },
+    {
+      id: 9,
+      seatCount: "50",
+      days30: "3140",
+      days90: "6940",
+      days180: "9160",
+      days360: "13700",
+    },
+  ];
+
   return (
     <Box sx={ManageSubscriptionStyles.root}>
-      <Typography
-        variant="h6"
-        component="div"
-        sx={ManageSubscriptionStyles.preSubHead}
-      >
-        Add Subscription Plan
-      </Typography>
+      <Box marginBottom={5} sx={ManageSubscriptionStyles.addSubscriptionButton}>
+        <Grid item xs={12} sm={6} md={2.2}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Subscription</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Subscription Type"
+              onChange={(e) => setSubscriptionType(e.target.value as string)}
+            >
+              <MenuItem value="hajj">Economy</MenuItem>
+              <MenuItem value="umrah">Delux</MenuItem>
+              <MenuItem value="umrah">Super Delux</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Button
+          color="primary"
+          variant="contained"
+          disabled={subscriptionType !== "" ? false : true}
+          onClick={handleManageSubscription}
+        >
+          Add Subscription
+        </Button>
+      </Box>
 
-      <Box sx={ManageSubscriptionStyles.program}>
-        <Grid container columns={{ xs: 1, sm: 4.6, md: 12 }}>
-          <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
-            <Typography variant="h6">Seat Count</Typography>
-            <TextField type="number" size="small" />
+      {show && (
+        <Grid
+          container
+          spacing={1}
+          sx={manageSubscriptionStyles.addSubscriptionForm}
+        >
+          <Grid item xs={12} sm={6} md={2.2}>
+            <TextField
+              fullWidth
+              type="number"
+              label="Seat Count"
+              variant="outlined"
+            />
           </Grid>
-
-          <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
-            <Typography variant="h6">30 Days Cost</Typography>
-            <TextField type="number" size="small" />
+          <Grid item xs={12} sm={6} md={2.2}>
+            <TextField
+              fullWidth
+              type="number"
+              label="30 Days"
+              variant="outlined"
+            />
           </Grid>
-
-          <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
-            <Typography variant="h6">90 Days Cost</Typography>
-            <TextField type="number" size="small" />
+          <Grid item xs={12} sm={6} md={2.2}>
+            <TextField
+              fullWidth
+              type="number"
+              label="90 Days"
+              variant="outlined"
+            />
           </Grid>
-
-          <Grid item xs={2.3} sx={ManageSubscriptionStyles.fair}>
-            <Typography variant="h6">180 Days Cost</Typography>
-            <TextField type="number" size="small" />
+          <Grid item xs={12} sm={6} md={2.2}>
+            <TextField
+              fullWidth
+              type="number"
+              label="180 Days"
+              variant="outlined"
+            />
           </Grid>
-
-          <Grid item xs={2.8} sx={ManageSubscriptionStyles.seats}>
-            <Typography variant="h6">360 Days Cost</Typography>
-            <TextField type="number" size="small" />
-            <Icon sx={ManageSubscriptionStyles.icon}>+</Icon>
+          <Grid item xs={12} sm={6} md={2.2}>
+            <TextField
+              fullWidth
+              type="number"
+              label="360 Days"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={1}>
+            <IconButton color="success" onClick={handleAddButtonClick}>
+              <AddCircleIcon />
+            </IconButton>
           </Grid>
         </Grid>
-      </Box>
-      <Box sx={ManageSubscriptionStyles.table}>
-        <table>
-          <thead>
-            <tr>
-              <th style={{ backgroundColor: "white" }}></th>
-              <th>Seat Count</th>
-              <th>30 Days</th>
-              <th>90 Days</th>
-              <th>180 Days</th>
-              <th>360 Days</th>
-              <th style={{ backgroundColor: "white" }}></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>50</td>
-              <td>5000</td>
-              <td>10000</td>
-              <td>15000</td>
-              <td>22500</td>
-              <td>✕</td>
-            </tr>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>100</td>
-              <td>47500</td>
-              <td>15000</td>
-              <td>22500</td>
-              <td>33750</td>
-              <td>✕</td>
-            </tr>
-            <tr>
-              <td>
-                <CreateIcon />
-              </td>
-              <td>5000</td>
-              <td>16875</td>
-              <td>33750</td>
-              <td>50625</td>
-              <td>75938</td>
-              <td>✕</td>
-            </tr>
-          </tbody>
-        </table>
-      </Box>
-      <Divider sx={ManageSubscriptionStyles.divider} />
+      )}
+      <br />
+      <DataGrid
+        keepNonExistentRowsSelected
+        sx={manageSubscriptionStyles.root}
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        rowSelection={false}
+        pageSizeOptions={[5, 10]}
+      />
+<Box sx={manageSubscriptionStyles.endButton}>
+  <Button
+          color="primary"
+          variant="contained"
+          disabled={subscriptionType !== "" ? false : true}
+          onClick={handleManageSubscription}
+        >
+          Update Plans
+        </Button>
+</Box>
+
     </Box>
   );
 };
