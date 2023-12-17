b307e520b7bb1a4e9fab6321a630c0522b82c956
amir.mursal <amir.mursal@nuance.com>
2023-12-17 16:31:20 +0530

    fix: added secure url for login api

--- src/services/login.ts
+++ src/services/login.ts
@@ -18,7 +18,7 @@ type LoginResponse = {
 export const loginApi = createApi({
   reducerPath: "loginApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://mahapuleayaz-001-site1.itempurl.com/api",
+    baseUrl: "https://mahapuleayaz-001-site1.itempurl.com/api",
   }),
   endpoints: (builder) => ({
     login: builder.mutation<LoginResponse, Login>({
